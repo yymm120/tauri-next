58831e780eafc5077c3efec3eafdf24c06177cb7
lxh <1208401731@qq.com>
2024-07-13 14:14:57 -0600

    init

--- src-next/src/app/layout.tsx
+++ src-next/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-// import "@/styles/globals.css";
 import './globals.css'
+import {WindowTitleBar} from '@/components/titlebar/WindowTitleBar';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <WindowTitleBar/>
+        {children}
+        </body>
     </html>
   );
 }
